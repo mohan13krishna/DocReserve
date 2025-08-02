c621853e71403733fdef4dda70faae653d2689c6
Mohan Krishna Thalla <142295454+mohan13krishna@users.noreply.github.com>
2025-08-02 15:54:20 +0530

    Create db.js

---
+++ Backend/models/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+const dbConfig = require('../config/db.config');
+
+const pool = mysql.createPool({
+  host: dbConfig.host,
+  user: dbConfig.user,
+  password: dbConfig.password,
+  database: dbConfig.database,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
