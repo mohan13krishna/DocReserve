838719ef712d9ef765b23d0e726313b7535489c8
Mohan Krishna Thalla <142295454+mohan13krishna@users.noreply.github.com>
2025-08-02 15:53:29 +0530

    Create authMiddleware.js

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+exports.authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+exports.authorizeRole = (roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Access denied: You do not have the necessary permissions.' });
+    }
+    next();
+  };
+};
